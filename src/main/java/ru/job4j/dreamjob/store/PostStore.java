4b884593760368b78aa20c448b4f02cb3ac368ea
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-10-05 12:21:48 +0500

    Refactor Post.

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -2,6 +2,8 @@ package ru.job4j.dreamjob.store;
 
 import ru.job4j.dreamjob.model.Post;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,9 +14,12 @@ public class PostStore {
 
 
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job"));
-        posts.put(2, new Post(2, "Middle Java Job"));
-        posts.put(3, new Post(3, "Senior Java Job"));
+        posts.put(1, new Post(1, "Junior Java Job", "Junior",
+                LocalDate.of(2013, Month.AUGUST, 20)));
+        posts.put(2, new Post(2, "Middle Java Job", "Middle",
+                LocalDate.of(2018, Month.DECEMBER, 4)));
+        posts.put(3, new Post(3, "Senior Java Job", "Senior",
+                LocalDate.of(2015, Month.JANUARY, 8)));
     }
     public static PostStore instOf() {
         return INST;
